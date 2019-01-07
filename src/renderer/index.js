bcf789ce88de4139bbef5fd75cb8245b6f34141d
59naga <i59naga@icloud.com>
2019-01-08 04:34:48 +0900

    :tulip: initial

---
+++ src/renderer/index.js
@@ -0,0 +1,22 @@
+import clipboardy from "clipboardy";
+
+const origin = "https://gbf-raid-stream-nszcupcclj.now.sh";
+
+document.head.innerHTML +=
+  "<style>body{margin:0}iframe{width:100vw;height:100vh}</style>";
+document.body.innerHTML = `<iframe src="${origin}" frameborder="0"></iframe>`;
+
+const iframe = document.querySelector("iframe");
+iframe.addEventListener("load", () => {
+  iframe.contentWindow.postMessage("handshake", "*");
+  window.addEventListener(
+    "message",
+    function(event) {
+      if (event.origin !== origin) {
+        return;
+      }
+      clipboardy.writeSync(event.data);
+    },
+    false
+  );
+});
