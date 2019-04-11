439b0552c3fe51d2a7d7f3d7abe9ac785e41571d
59naga <i59naga@icloud.com>
2019-04-11 20:40:03 +0900

    :arrow_up: gbf-data@0.0.27に固定

--- src/store.js
+++ src/store.js
@@ -7,7 +7,7 @@ import createPersistedState from 'vuex-persistedstate'
 import isMobile from 'is-mobile'
 import Visibility from 'visibilityjs'
 
-const jsonBossesUrl = 'https://unpkg.com/gbf-data/dist/raid.json'
+const jsonBossesUrl = 'https://unpkg.com/gbf-data@0.0.27/dist/raid.json'
 const gbfRaidServerUrl = 'http://v157-7-52-20.z1d20.static.cnode.jp/'
 
 function fetchCache(gbfRaidServer) {
